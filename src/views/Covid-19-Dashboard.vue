b2431721dcadfe46b451ec3844a3c229e592624b
niko <nikodem.bienia@envelio.de>
2021-04-16 20:20:04 +0200

    Covid Country info

---
+++ src/views/Covid-19-Dashboard.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h1>Countries</h1>
+        <div v-for="country in listOfCountries" :key="country" class="country">
+            <h3>{{country}}</h3>
+            <div>Confirmed: {{getCountryConfirmed(country)}}</div>
+            <div>Recovered: {{getCountryRecovered(country)}}</div>
+            <div>Deaths: {{getCountryDeaths(country)}}</div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { onMounted, ref, computed } from "vue";
+import { defineComponent } from 'vue'
+import axios from "axios";
+
+export default defineComponent({
+    setup() {
+        const countries = ref({} as any);
+        const getCoronaData = async () => {
+            const {data} = await axios.get("https://covid-api.mmediagroup.fr/v1/cases")
+            countries.value = data;
+        }
+        const listOfCountries = computed(() => {
+            const listOfCountries = Object.keys(countries.value);
+            return listOfCountries
+        })
+
+        const getCountryConfirmed = (country: string) => {
+            return countries.value[country]["All"]["confirmed"];
+        }
+
+        const getCountryRecovered = (country: string) => {
+            return countries.value[country]["All"]["recovered"];
+        }
+
+        const getCountryDeaths = (country: string) => {
+            return countries.value[country]["All"]["deaths"];
+        }
+
+        onMounted(getCoronaData);
+
+        return {
+            countries,
+            listOfCountries,
+            getCoronaData,
+            getCountryConfirmed,
+            getCountryRecovered,
+            getCountryDeaths
+        }
+    }
+});
+</script>
+
+<style scoped>
+.country {
+    margin-bottom: 2rem;
+}
+</style>
